2d6d2325dc2c9e82311f1f1973f4a4c0dca652cf
Anthony Hill <hillan1152@gmail.com>
2022-01-29 14:33:29 -0500

    fixed URL issue with cheskout session, will need to check backend for more sdisplaying more specific errors for user

--- src/Form/Confirmation.js
+++ src/Form/Confirmation.js
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useEffect }from 'react';
+import axios from 'axios';
 import { Form, Input, Button, InputNumber, Switch, Select, Radio } from 'antd';
 import { InfoCircleOutlined } from '@ant-design/icons';
 
 export default function Confirmation(props) {
     let { prospect } = props;
     const [form] = Form.useForm();
-
+    const baseURL = "http://localhost:5000"
     let package_type = {
         0: "Standard",
         1: "Bronze",
         2: "Silver"
+    };
+
+    const onFinish = (values) => {
+        if(values.package == "Standard"){
+            // If user wants the free package (standard)
+            console.log("values", values.package)
+            axios.post(`${baseURL}/api/prospects`, values)
+                .then(res => console.log("res", res))
+                .catch(err => console.log("err", err.message))
+            // Push directly to database
+        } else {
+            fetch(`${baseURL}/api/stripe/create-checkout-session`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                }, 
+                body: JSON.stringify({
+                    packages: [
+                        { package: values.package, quantity: 1 }
+                    ],
+                }),
+            })
+            .then(res => {
+                if(res.ok) return res.json()
+                return res.json().then(json => Promise.reject(json))
+            })
+            .then(({ url }) => {
+                window.location = url
+            }).catch(e => {
+                console.error(e.error)
+            })
+
+        }
+            // Else Go to payment page for processing
+            // 1. Payment is approved
+            // 2. add to data base
+
+
+
+
     };
 
     return (
@@ -41,7 +82,7 @@ export default function Confirmation(props) {
                     gpa: prospect.gpa,
                     package: prospect.package !== null ? `${package_type[prospect.package]}` : '',
                 }}
-                // onFinish={onFinish}
+                onFinish={onFinish}
             >
                 <Form.Item 
                     label="First Name" 
@@ -171,7 +212,9 @@ export default function Confirmation(props) {
                 <Form.Item >
                     <div style={{ display: "flex", justifyContent: 'space-evenly'}}>
                         <Button htmlType="cancel">Cancel</Button>
-                        <Button type="primary" htmlType="submit">To Payment</Button>
+                        <Button type="primary" htmlType="submit">
+                            {prospect.package == 0 ? 'Submit' : "To Payment"}
+                        </Button>
                     </div>
                 </Form.Item>
             </Form>

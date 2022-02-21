872e22a18f3dd1acd064c74abaab992f70f8b4e7
Anthony Hill <hillan1152@gmail.com>
2022-02-20 20:11:27 -0500

    fixed the Packaging portion and need to style the cards better

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -5,42 +5,59 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Welcome() {
     const navigate = useNavigate();
-
     return (
         <div className='welcome-div'>
             <h1>Next Big Prospect</h1>
             <h4>Click to Start Ranking</h4>
             <Button type="primary" onClick={() => navigate('/signup')}>Sign Up</Button>
             <section className="packages">
-                <h2>Package Type</h2>
+                <h2 style={{ fontSize: '2rem', marginTop: '2%'}}>Package Type</h2>
                 <section className='package-align'>
-                    <Card title="Free">
-                        {/* <h3>Price: Free</h3> */}
-                        <List.Item>
-                            <Typography.Text>Description: Adds you to our database and prospect list.</Typography.Text>
+                    <Card className='package' bordered>
+                        <List.Item className='card-title'>
+                            <Typography.Text><b>Free</b></Typography.Text>
                         </List.Item>
                         <List.Item>
-                            <Typography.Text>Added to prospect list</Typography.Text>
+                            <Typography.Text><b>Price:</b> Free</Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text>On Prospect Grading List</Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text>Added to Database</Typography.Text>
                         </List.Item>
                     </Card>
-                    <div className='single-package'>
-                        <h2>Standard</h2>
-                        <h3>Price: $14.99</h3>
-                        <p>Description: Guarenteed Ranking and Shoutout</p>
-                        <ul>
-                            <li>Guarenteed Ranking within 5 Days</li>
-                            <li>Shoutout on Social Media</li>
-                        </ul>
-                    </div>
-                    <div className='single-package'>
-                        <h2>Advanced</h2>
-                        <h3>Price: $24.99</h3>
-                        <p>Description: Ranking and Writeup</p>
-                        <ul>
-                            <li>All Items from Standard Package</li>
-                            <li>Prospect Writeup</li>
-                        </ul>
-                    </div>
+                    <Card className='package' bordered>
+                        <List.Item className='card-title'>
+                            <Typography.Text><b>Standard</b></Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text><b>Price:</b> $14.99</Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text>Guarenteed Ranking Within 5 Days</Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text>Added to Database</Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text>Shoutout on Social Media</Typography.Text>
+                        </List.Item>
+                    </Card>
+                    <Card className='package' bordered>
+                        <List.Item className='card-title'>
+                            <Typography.Text><b>Advanced</b></Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text><b>Price:</b> $24.99</Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text>All Standard Package Items</Typography.Text>
+                        </List.Item>
+                        <List.Item>
+                            <Typography.Text>Prospect Writeup</Typography.Text>
+                        </List.Item>
+                    </Card>
                 </section>
             </section>
         </div>
